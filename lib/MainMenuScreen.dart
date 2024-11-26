c7c357cce9221933e6a96f0ef503155898139839
NatillasConNata <n.martinezc.2020@alumnos.urjc.es>
2024-11-26 09:41:40 +0100

    main menu almost finished

--- lib/MainMenuScreen.dart
+++ lib/MainMenuScreen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:gif/gif.dart';
 //https://pub.dev/documentation/animated_glitch/latest/topics/Without%20shader-topic.html
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/foundation.dart'; // Importar para kIsWeb
 import 'package:gif/gif.dart';
+import 'dart:io' ;
+import 'GameScreen.dart';
 
 class MenuPrincipal extends StatefulWidget {
   const MenuPrincipal({Key? key}) : super(key: key);
@@ -15,7 +19,7 @@ class MenuPrincipal extends StatefulWidget {
 
 class _MenuPrincipalState extends State<MenuPrincipal> with SingleTickerProviderStateMixin {
   late AnimationController _controller;
-  late GifController _gifController;
+//  late GifController _gifController;
 
 
   @override
@@ -29,13 +33,13 @@ class _MenuPrincipalState extends State<MenuPrincipal> with SingleTickerProvider
     )..repeat(); // Looping para la animación
 
     // Inicializa el GifController
-    _gifController = GifController(vsync: this);
+//    _gifController = GifController(vsync: this);
   }
 
   @override
   void dispose() {
     _controller.dispose();
-    _gifController.dispose();
+//    _gifController.dispose();
     super.dispose();
   }
 
@@ -46,14 +50,13 @@ class _MenuPrincipalState extends State<MenuPrincipal> with SingleTickerProvider
 
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text('HAnGMaN GAME')),
+        //title: Center(child: Text('HAnGMaN GAME')),
       ),
           body: Stack(
           children: [
           // Widget que muestra el GIF de fondo
           Positioned.fill(
-          child: Image.asset(
-          'assets/HungManBackground01.gif',
+          child: Image.asset('assets/Gifs/HungManBackground01.gif',
           fit: BoxFit.cover, // Para que ocupe toda la pantalla
           ),
           ),
@@ -61,12 +64,44 @@ class _MenuPrincipalState extends State<MenuPrincipal> with SingleTickerProvider
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
+
+                Container(
+                  //height: screenHeight * 0.2,
+                  child: Center(
+                    child: Stack( // Usamos Stack para superponer los textos
+                    alignment: Alignment.center,
+                    children: [
+                      Text(
+                        'HAnGMaN GAME',
+                        style: TextStyle(
+                          fontSize: 40,
+                          foreground: Paint()
+                            ..style = PaintingStyle.stroke
+                            ..strokeWidth = 6
+                            ..color = Colors.blue[700]!,
+                        ),
+                      ),
+                      Text(
+                        'HAnGMaN GAME', // Relleno del texto de arriba
+                        style: TextStyle(
+                          fontSize: 40,
+                          color: Colors.grey[300],
+                        ),
+                      ),
+                    ],
+                    ),
+                  ),
+                ),
+
                 Container(
                   width: screenWidth * 0.4,
                   height: screenHeight * 0.2,
                   child: ElevatedButton(
                     onPressed: () {
                       // Acción para jugar
+                      Navigator.push(context, 
+                      MaterialPageRoute(builder: (context) => const GameUI()),
+                      );
                     },
                     child: const Text('JUGAR'),
                   ),
@@ -78,21 +113,39 @@ class _MenuPrincipalState extends State<MenuPrincipal> with SingleTickerProvider
                   child: ElevatedButton(
                     onPressed: () {
                       // Acción para ranking
+                      /*Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => const Ranking()),
+                      );*/
                     },
                     child: const Text('RANKING'),
                   ),
                 ),
                 const SizedBox(height: 20),
-                Container(
-                  width: screenWidth * 0.4,
-                  height: screenHeight * 0.2,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // Acción para salir
-                    },
-                    child: const Text('SALIR'),
+                //if (Platform.isAndroid || Platform.isIOS)
+                  if (!kIsWeb) //comprobar si esta ejecutando en chrome, si es asi, no muestra el boton
+                  Container(
+                    width: screenWidth * 0.4,
+                    height: screenHeight * 0.2,
+
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Acción para salir
+                        if (Platform.isAndroid || Platform.isIOS) {//si está en android/ios, cierra
+                          // En Android y iOS
+                          SystemNavigator
+                              .pop(); // Se recomienda usar esto para cerrar la aplicación
+                        } else {//si está en desktop, pone pestaña en negro
+                          // En Web y otras plataformas
+                          Navigator.pop(
+                              context); // Vuelve a la pantalla anterior o puedes usar Navigator.of(context).maybePop();
+                        }
+                      },
+                      child: const Text('SALIR'),
+                    ),
                   ),
-                ),
+
+
+
               ],
             ),
           ),
--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           useMaterial3: true,
         ),
-        home: GameUI(),
+        home: MenuPrincipal(),
       ),
     );
   }
