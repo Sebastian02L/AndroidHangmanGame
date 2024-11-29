065ec80aacc2d27ce893da56dd9430ad2ab5c43e
carlossantayana <carlossantayanavicente@gmail.com>
2024-11-29 04:02:20 +0100

    Ajustes responsivos

--- lib/GameSettingsScreen.dart
+++ lib/GameSettingsScreen.dart
@@ -18,7 +18,7 @@ class _GameSettingsUIState extends State<GameSettingsUI> {
   final TextEditingController playerNameController = TextEditingController();
 
   @override
-  void dispose(){
+  void dispose() {
     playerNameController.dispose();
     super.dispose();
   }
@@ -38,208 +38,205 @@ class _GameSettingsUIState extends State<GameSettingsUI> {
     var appState = context.watch<GameState>();
 
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(height: 60),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      PlayAudio("Click.mp3", 0);
-                    },
-                    child: Icon(Icons.arrow_back, size: 30.0),
-                    style: ElevatedButton.styleFrom(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 20, vertical: 20)),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
                     padding: const EdgeInsets.all(10.0),
-                    child: Card(
-                      color: theme.colorScheme.primary,
-                      child: Padding(
-                        padding: const EdgeInsets.all(20),
-                        child: Text(
-                          "Ajustes de la partida",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontSize: 30, color: theme.colorScheme.onPrimary),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        PlayAudio("Click.mp3", 0);
+                        Navigator.pop(context);
+                      },
+                      child: Icon(Icons.arrow_back, size: 30.0),
+                      style: ElevatedButton.styleFrom(
+                          padding:
+                              EdgeInsets.symmetric(horizontal: 10, vertical: 10)),
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Card(
+                        color: theme.colorScheme.primary,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            "Ajustes de la partida",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 22, color: theme.colorScheme.onPrimary),
+                          ),
                         ),
                       ),
                     ),
                   ),
+                ],
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.2,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    buildSectionTitle(theme, "Categoría de las palabras"),
+                    DropdownButton(
+                      value: selectedCategory,
+                      hint: Text("Seleccione una categoría...",
+                          style: TextStyle(fontSize: 20)),
+                      items: options.map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value, style: TextStyle(fontSize: 20)),
+                        );
+                      }).toList(),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          selectedCategory = newValue;
+                        });
+                      },
+                    ),
+                  ],
                 ),
-              ],
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Card(
-                      color: theme.colorScheme.primary,
-                      child: Padding(
-                        padding: const EdgeInsets.all(18),
-                        child: Text("Categoría de las palabras",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                fontSize: 30,
-                                color: theme.colorScheme.onPrimary)),
+              ),
+              SizedBox(height: 10),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.2,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    buildSectionTitle(theme, "Modo de juego"),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        buildModeButton(
+                          theme,
+                          () {
+                            PlayAudio("Click.mp3", 0);
+                            setState(() {
+                              selectedMode = "Fast";
+                            });
+                          },
+                          "Fast",
+                          "Rápido",
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        buildModeButton(
+                          theme,
+                          () {
+                            PlayAudio("Click.mp3", 0);
+                            setState(() {
+                              selectedMode = "Marathon";
+                            });
+                          },
+                          "Marathon",
+                          "Maratón",
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.2,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    buildSectionTitle(theme, "Nombre del jugador"),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: TextField(
+                        controller: playerNameController,
+                        decoration: InputDecoration(
+                          labelText: "Nombre del jugador",
+                          hintText: "Ingrese su nombre...",
+                          border: OutlineInputBorder(),
+                        ),
                       ),
                     ),
-                  ),
+                  ],
                 ),
-              ],
-            ),
-            DropdownButton(
-              value: selectedCategory,
-              hint: Text("Seleccione una categoría...",
-                  style: TextStyle(fontSize: 25)),
-              items: options.map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value, style: TextStyle(fontSize: 25)),
-                );
-              }).toList(),
-              onChanged: (String? newValue) {
-                setState(() {
-                  selectedCategory = newValue;
-                });
-              },
-            ),
-            SizedBox(height: 20),
-            Row(
-              children: [
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Card(
-                      color: theme.colorScheme.primary,
-                      child: Padding(
-                        padding: const EdgeInsets.all(18),
-                        child: Text("Modo de juego",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                fontSize: 30,
-                                color: theme.colorScheme.onPrimary)),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    PlayAudio("Click.mp3", 0);
-                    setState(() {
-                      selectedMode = "Fast";
-                    });
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: selectedMode == "Fast" ? Colors.grey[400] : null,
-                    side: selectedMode == "Fast" ? BorderSide(color: theme.colorScheme.primary, width: 5) : null,
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text("Rápido",
-                        style: TextStyle(
-                            fontSize: 30, color: theme.colorScheme.primary)),
-                  ),
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    PlayAudio("Click.mp3", 0);
-                    setState(() {
-                      selectedMode = "Marathon";
-                    });
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: selectedMode == "Marathon" ? Colors.grey[400] : null,
-                    side: selectedMode == "Marathon" ? BorderSide(color: theme.colorScheme.primary, width: 5) : null,
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text("Maratón",
-                        style: TextStyle(
-                            fontSize: 30, color: theme.colorScheme.primary)),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
-            Row(
-              children: [
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Card(
-                      color: theme.colorScheme.primary,
-                      child: Padding(
-                        padding: const EdgeInsets.all(18),
-                        child: Text("Nombre del jugador",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                fontSize: 30,
-                                color: theme.colorScheme.onPrimary)),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  width: 400,
-                  child: TextField(
-                    controller: playerNameController,
-                    decoration: InputDecoration(
-                      labelText: "Nombre del jugador",
-                      hintText: "Ingrese su nombre...",
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                PlayAudio("Click.mp3", 0);
-                String playerName = playerNameController.text;
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.2,
+                child: Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      PlayAudio("Click.mp3", 0);
+                      String playerName = playerNameController.text;
 
-                if (playerName != null && selectedCategory != null && selectedMode != null) {
-                  appState.setPlayerName(playerName);
-                  appState.setSelectedCategory(selectedCategory);
-                  appState.setSelectedMode(selectedMode);
-                  Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => const GameUI()),
-                  );
-                }
-              },
+                      if (playerName != null &&
+                          selectedCategory != null &&
+                          selectedMode != null) {
+                        appState.setPlayerName(playerName);
+                        appState.setSelectedCategory(selectedCategory);
+                        appState.setSelectedMode(selectedMode);
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const GameUI()),
+                        );
+                      }
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("¡Comenzar Partida!",
+                          style: TextStyle(
+                              fontSize: 24, color: theme.colorScheme.primary)),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildSectionTitle(ThemeData theme, String text) {
+    return Row(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Card(
+              color: theme.colorScheme.primary,
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Text("¡Comenzar Partida!",
+                child: Text(text,
+                    textAlign: TextAlign.center,
                     style: TextStyle(
-                        fontSize: 30, color: theme.colorScheme.primary)),
+                        fontSize: 22, color: theme.colorScheme.onPrimary)),
               ),
             ),
-          ],
+          ),
         ),
+      ],
+    );
+  }
+
+  Widget buildModeButton(ThemeData theme, void Function() onPressed, String type, String text) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: selectedMode == type ? Colors.grey[400] : null,
+        side: selectedMode == type
+            ? BorderSide(color: theme.colorScheme.primary, width: 5)
+            : null,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(text,
+            style: TextStyle(fontSize: 20, color: theme.colorScheme.primary)),
       ),
     );
   }
